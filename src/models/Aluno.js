46f4810144d173f556aa69a0ee71af8416813331
Lucas Marques <lucasdejesusmarques2005@gmail.com>
2026-09-01 12:23:11 -0300

    feat: adiciona a classe Aluno

---
+++ src/models/Aluno.js
@@ -0,0 +1,11 @@
+/**
+ * Representa um aluno cadastrado no sistema.
+ */
+export default class Aluno {
+    constructor(id, nome, email, matricula){
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+    }
+}
