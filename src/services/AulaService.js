139648ccfc4fd71a926358a5f2d1672e507313a3
Lucas Marques <lucasdejesusmarques2005@gmail.com>
2026-09-04 15:56:15 -0300

    feat: adiciona remoção de aula

--- src/services/AulaService.js
+++ src/services/AulaService.js
@@ -20,8 +20,13 @@ export default class AulaService {
     }
     static atualizar(id, aula) {
         const aulas = AulaService.listar();
-        const indice = aulas.findIndex(aula => aula.id === id);
-        aulas[indice] = aula;
+        const indice  = aulas.findIndex(aula => aula.id === id);
+        aulas[indice ] = aula;
         StorageService.atualizar("aulas", aulas);
+    }
+    static remover(id) {
+        const aulas = AulaService.listar();
+        const remocao = aulas.filter(aula => aula.id !== id);
+        StorageService.atualizar("aulas", remocao);
     }
 }
