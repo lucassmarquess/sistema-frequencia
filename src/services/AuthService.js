cbf601f543ea8a7cb6471733cbcabe7cc1494f6e
Lucas Marques <lucasdejesusmarques2005@gmail.com>
2026-09-06 12:21:14 -0300

    feat: adiciona login no AuthService

---
+++ src/services/AuthService.js
@@ -0,0 +1,17 @@
+/**
+ * Responsável por realizar operações relacionadas a autenticação.
+ */
+import StorageService from "./StorageService";
+import UsuarioService from "./UsuarioService";
+
+export default class AuthService{
+    static login(email) {
+        const usuarios = UsuarioService.listar();
+        const autenticacao = usuarios.find(usuario => usuario.email === email);
+        if (autenticacao === undefined) {
+            return "Usuário não identificado";
+        } 
+        StorageService.atualizar("usuarioLogado", autenticacao);
+        return "Login realizado com sucesso";
+    }
+}
