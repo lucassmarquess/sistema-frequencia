573a37dcfdb9b32ed8a50cad45c50f75709036e9
Lucas Marques <lucasdejesusmarques2005@gmail.com>
2026-09-09 12:35:26 -0300

    feat: adiciona tela de login

---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<script>
+    import AuthService from "../services/AuthService";
+    
+    export default {
+        data () {
+            return {
+                email: "",
+                mensagem: ""
+            };
+        },
+        methods: {
+            login () {
+                this.mensagem = AuthService.login(this.email);
+
+            }
+        }
+    }
+</script>
+<template>
+    <div>
+        <h1>Sistema de Frequência</h1>
+
+        <input type="email" placeholder="E-mail" v-model="email">
+
+        <button @click="login">Entrar</button>
+        {{ mensagem }}
+    </div>
+</template>
